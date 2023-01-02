e188fe73f305e47c430ccbaca2f09602d0a85f8d
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-01-02 02:20:48 -0800

    event add list item

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ const select = (q) => { return (d.querySelectorAll(q).length <= 1) ? d.querySele
 const todoInput = select('.form-todo input'),
     addTodo = select('.add-todo'),
     todoItems = select('.todo-items');
+
+// Event
+addTodo.addEventListener('click', addTodoHandler);
+
+// Functions
+function addTodoHandler(e) {
+
+    e.preventDefault(); // Disable Submit Form
+
+    if (!todoInput.value.trim().length) // Checks that the input is empty
+
+        alert("Please Write Input") // If the entry is empty, this message will be displayed.
+
+    else {
+        const todoItemsDiv = d.createElement('div');
+
+        todoItemsDiv.innerHTML = `<li>${todoInput.value}</li><span><i class="far fa-square-check"></i></span><span><i class="fa-regular fa-trash-can"></i></span>`
+
+        todoItemsDiv.classList.add('list-todo');
+
+        todoItems.appendChild(todoItemsDiv);
+
+        todoInput.value = "";
+    }
+
+}
