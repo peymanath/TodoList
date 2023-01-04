58e4ca9170066591e1ca14f9dfbff7f9307bd59b
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-01-04 09:08:45 -0800

    added filter section js code

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ const todoInput = select('.form-todo input');
 const addTodo = select('.add-todo');
 const todoItems = select('.todo-items');
 const listTodo = select('.list-todo');
+const filters = select('.filters');
 
 // Event
 addTodo.addEventListener('click', addTodoHandler); // Adding event to todo button after click
-todoItems.addEventListener('click', checkRemove); // Adding event to todo button after click
+todoItems.addEventListener('click', checkRemove); // Adding event to icon after click
+filters.addEventListener('click', filter); // Adding event to filters after click
 
 // Functions
 function addTodoHandler(e) {
@@ -42,11 +44,31 @@ function checkRemove(e) {
     const classLis = [...e.target.classList];
     const Parent = e.target.parentElement.parentElement;
 
-    if (classLis.find((e) => e == "remove")) {
+    if (classLis.find(e => e == "remove")) {
         Parent.remove();
     };
-    if (classLis.find((e) => e == "check")) {
+    if (classLis.find(e => e == "check")) {
         Parent.classList.toggle("completed");
     };
+
+}
+
+function filter(e) {
+
+    const childListItems = [...todoItems.childNodes]
+
+    const filterValue = e.target.value;
+
+    childListItems.forEach(e => {
+       
+        if (filterValue == "2")
+        e.classList.contains('completed') ? e.style.display = 'flex' : e.style.display = 'none' ;
+
+        else if (filterValue == "3")  
+        !e.classList.contains('completed')? e.style.display = 'flex' : e.style.display = 'none' ;
+
+        else e.style.display = 'flex';
+
+    });
 
 }
